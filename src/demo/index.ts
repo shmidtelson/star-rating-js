236dc92cba596433e2b1c0083717810c1314d36a
Romuald Shmidtelson <d-w-b@mail.ru>
2021-07-13 12:53:13 +0300

    Updated demo

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,11 +1,18 @@
 import StarRating from "../lib";
 
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 const sr = new StarRating(document.getElementById('root'), {});
-window.sr2 = new StarRating(document.getElementById('secondPlugin'), {
-    currentRating: 4,
-    onChange: () => {
-        console.log("I did it")
-    }
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+let sr2 = new StarRating(document.getElementById('secondPlugin'), {
+  currentRating: 4,
 });
+
+sr2.onChange = (e) => {
+  console.log(e)
+  console.log("I did it")
+}
 
